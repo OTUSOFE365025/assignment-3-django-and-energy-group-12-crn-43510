60040673fe78d12210af4c7fe73cbae0c9916f25
ReidHillis <reidhillis7@gmail.com>
2025-11-09 15:18:57 -0500

    Completed part a

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ from db.models import *
 """ Replace the code below with your own """
 
 # Seed a few users in the database
-User.objects.create(name='Dan')
-User.objects.create(name='Robert')
+Product.objects.get_or_create(upc="12345", name="Cereal", price=4.47)
+Product.objects.get_or_create(upc="67890", name="Bread", price=2.48)
+Product.objects.get_or_create(upc="90210", name="Eggs", price=3.93)
+Product.objects.get_or_create(upc="29577", name="Tea", price=2.90)
+Product.objects.get_or_create(upc="41410", name="Grapes", price=6.70)
+Product.objects.get_or_create(upc="89283", name="Donuts", price=2.00)
 
-for u in User.objects.all():
-    print(f'ID: {u.id} \tUsername: {u.name}')
+for u in Product.objects.all():
+    print(f'Name: {u.name} \tUPC: {u.upc}  \tPrice: {u.price}')
